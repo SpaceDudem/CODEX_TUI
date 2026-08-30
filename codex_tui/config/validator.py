c1b001e4b96b845607b1dfc59434981e582852e4
Eric Korhonen <sd.m@tutanota.com>
2026-08-30 14:45:27 -0500

    lint: modernize validator imports

--- codex_tui/config/validator.py
+++ codex_tui/config/validator.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
 from pathlib import Path
-from typing import Any, Mapping
+from typing import Any
 
 from jsonschema.exceptions import SchemaError
 from jsonschema.validators import validator_for
